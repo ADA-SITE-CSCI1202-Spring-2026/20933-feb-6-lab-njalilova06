a79bc1a3e95b774ef40d8b52eb96a683d91179f4
njalilova06 <njalilova20313@ada.edu.az>
2026-02-06 13:59:35 +0400

    work done now

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
-//public class Main {
-//    public static void main(String[] args) {
-//
-//        int num1 = Integer.parseInt(args[0]);
-//        int num2 = Integer.parseInt(args[1]);
-//
-//        System.out.println("Sum = " + (num1 + num2));
-//        System.out.println("Subtraction = " + (num1 - num2));
-//        System.out.println("Multiplication = " + (num1 * num2));
-//
-//        if (num2 != 0) {
-//            System.out.println("Integer Division = " + (num1 / num2));
-//            System.out.println("Remainder = " + (num1 % num2));
-//        } else {
-//            System.out.println("Division and remainder not possible");
-//        }
-//    }
-//}
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
