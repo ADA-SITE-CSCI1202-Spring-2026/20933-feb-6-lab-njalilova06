d62b9958c844a1959a57fdac47b6a9c67dc9c6b6
njalilova06 <njalilova20313@ada.edu.az>
2026-02-06 13:56:26 +0400

    Exercise2

---
+++ src/Exercise3.java
@@ -0,0 +1,21 @@
+//import java.util.Scanner;
+//public class Exercise3 {
+//    public static void main(String[] args){
+//
+//            Scanner input = new Scanner(System.in);
+//
+//            System.out.print("Enter total seconds: ");
+//            int totalSeconds = input.nextInt();
+//
+//            int hours = totalSeconds / 3600;
+//            int minutes = (totalSeconds % 3600) / 60;
+//            int seconds = totalSeconds % 60;
+//
+//            System.out.println("Hours = " + hours);
+//            System.out.println("Minutes = " + minutes);
+//            System.out.println("Seconds = " + seconds);
+//
+//        }
+//
+//    }
+//
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int num1 = Integer.parseInt(args[0]);
+//        int num2 = Integer.parseInt(args[1]);
+//
+//        System.out.println("Sum = " + (num1 + num2));
+//        System.out.println("Subtraction = " + (num1 - num2));
+//        System.out.println("Multiplication = " + (num1 * num2));
+//
+//        if (num2 != 0) {
+//            System.out.println("Integer Division = " + (num1 / num2));
+//            System.out.println("Remainder = " + (num1 % num2));
+//        } else {
+//            System.out.println("Division and remainder not possible");
+//        }
+//    }
+//}
